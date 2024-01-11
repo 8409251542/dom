c0a188bc88b0b945ea74df13802a6cfbc84bf59a
Ravi Shankar <ravi.yoof@gmail.com>
2024-01-12 00:04:02 +0530

    done

---
+++ scripts.js
@@ -0,0 +1,10 @@
+let cnt = document.getElementById("container");
+//console.log(cnt);
+cnt.style.backgroundColor = "yellow";
+cnt.style.margin="20px";
+cnt.style.padding="10px";
+cnt.style.fontSize="18px";
+cnt.style.color="blue";
+cnt.style.fontWeight="bold";
+cnt.style.height="200px";
+cnt.style.width="300px";
